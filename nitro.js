9b3acd735b0d9962e51479c652f7a6d69fb8056e
fl3xpl0it <yngkuroko61@gmail.com>
2019-04-01 15:39:54 +0300

    Update nitro.js

--- nitro.js
+++ nitro.js
@@ -34,7 +34,7 @@ function spamla(){
 	message.channel.send("https:/"+"/discord.gift/" + rasteleSembol(16,'0aA'));
 }
 
-	if (message.channel.id = "CHANNEL ID"){
+	if (message.channel.id = "ID NUMARASI"){
 		if(message.content.startsWith("hrr!nitro ")){
 		try{
 		var veri = message.content.toString();
