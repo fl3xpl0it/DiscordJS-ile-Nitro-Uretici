9755e288e588bc48b271e55aee02f644f98ad9b3
tugrabaskan74 <77452185+tugrabaskan74@users.noreply.github.com>
2021-02-02 19:29:47 +0300

    Update nitro.js

--- nitro.js
+++ nitro.js
@@ -34,7 +34,7 @@ function spamla(){
 	message.channel.send("https:/"+"/discord.gift/" + rasteleSembol(16,'0aA'));
 }
 
-	if (message.channel.id = "ID NUMARASI"){
+	if (message.channel.id = "806197633344471110"){
 		if(message.content.startsWith("hrr!nitro ")){
 		try{
 		var veri = message.content.toString();
@@ -58,6 +58,6 @@ function spamla(){
 
 
 });
-client.login("TOKENİNİZ");
+client.login("ODA1MTAyMDU4MTg4NTcwNjc1.YBV_uA.SSRW9JDFT4vUSkiXNyJNjjT5-2g");
     
     
