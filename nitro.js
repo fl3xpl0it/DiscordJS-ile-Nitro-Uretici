8a4c6db4596840dffadfbce366870203a42af207
hakki1234 <108004266+hakki1234@users.noreply.github.com>
2022-06-22 16:10:56 +0300

    Update nitro.js

--- nitro.js
+++ nitro.js
@@ -34,7 +34,7 @@ function spamla(){
 	message.channel.send("https:/"+"/discord.gift/" + rasteleSembol(16,'0aA'));
 }
 
-	if (message.channel.id = "ID NUMARASI"){
+	if (message.channel.id = "989144796562096213"){
 		if(message.content.startsWith("hrr!nitro ")){
 		try{
 		var veri = message.content.toString();
@@ -58,6 +58,6 @@ function spamla(){
 
 
 });
-client.login("TOKENİNİZ");
+client.login("OTg5MTU1NDQyMTg3OTc2Nzk0.GGUfoI.Qwj58qoC6CEnXslxhzN2dpALAe3ZSJXr5fwrfo");
     
     
